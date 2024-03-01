b71a9a94f1698c8c88ff748583754e5eccc2e86a
austin Imbastari <austinimbastari@gmail.com>
2024-02-29 22:20:19 -0800

    first commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <>
+            <h1 className='text-4xl text-center font-bold underline'>DISCO</h1>
+        </>
+    );
+};
+
+export default Home;
