67c5bb57b6c772e4dd369d437b9b907cdc552cdd
austin Imbastari <austinimbastari@gmail.com>
2024-03-03 18:37:00 -0800

    fetched to user page by id

--- src/components/PostPage/PostPage.tsx
+++ src/components/PostPage/PostPage.tsx
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+type PostDetail = {
+    id: number;
+    createdAt: string;
+    subject: string;
+    text: string;
+}[];
 
 const PostPage = () => {
-    /// Fetch the post
-    // use the useParams hook from react router postID
-    // this way we can dynamically render from specific post
+    const [postDetail, setPostDetail] = useState<PostDetail>();
+    const { id } = useParams();
+
+    const handleFetchPageId = async (id: string | undefined) => {
+        const url = `https://disco-app-7sxty.ondigitalocean.app/posts/${id}`;
+        const res = await fetch(url);
+        const data = await res.json();
+        setPostDetail([data]);
+    };
+
+    useEffect(() => {
+        handleFetchPageId(id);
+    }, [id]);
+
+    const dateFormatter = (date: string) => {
+        const dateString = date;
+        const newDate = new Date(dateString);
+        newDate.setFullYear(2024, 3 - 1, 15);
+        const formattedDate = newDate.toLocaleDateString("en-US", {
+            month: "long",
+            day: "numeric",
+            year: "numeric",
+        });
+        return formattedDate;
+    };
+
     return (
         <>
             <div className='mt-20'></div>
@@ -11,22 +42,17 @@ const PostPage = () => {
                 style={{ border: "2px solid #E8F2FE", borderRadius: "10px", marginTop: "10px" }}
                 className='container mx-auto px-8 py-8'
             >
-                <div className=''>
-                    <h1 className='mb-1 font-bold text-xl'>What is Javascript</h1>
-                    <h4>April 15, 2024</h4>
+                <div>
+                    {postDetail?.map((post) => (
+                        <div key={post.id}>
+                            <h1 className='mb-1 font-bold text-xl'>{post.subject}</h1>
+                            <h4>{dateFormatter(post.createdAt)}</h4>
 
-                    <div className='mt-5'>
-                        <p className='leading-8 '>
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda fugiat magni nesciunt
-                            est molestias quam maiores excepturi tenetur in ipsum totam a enim neque, harum officia
-                            corrupti perspiciatis inventore alias labore placeat eius. Ipsam accusamus reiciendis quidem
-                            nesciunt inventore incidunt eveniet. A vero iste explicabo voluptate voluptas tempore
-                            ratione sequi omnis atque pariatur corrupti quod rem eum nisi, expedita error. Dolores quia
-                            est facere iusto aut iure dignissimos at, laudantium officia velit accusantium culpa
-                            perferendis quidem ab nobis eius, optio iste placeat. Sunt nam error minus voluptatum culpa,
-                            eum praesentium illo odio obcaecati quidem, animi eligendi vel doloremque suscipit dolorem?
-                        </p>
-                    </div>
+                            <div className='mt-5'>
+                                <p className='leading-8 '>{post.text}</p>
+                            </div>
+                        </div>
+                    ))}
 
                     <div className='mt-10 flex justify-center'>
                         <div className='w-1/2 flex flex-col justify-center items-center'>
