682646ee8589a06ca8fa2dcdc8b4cd57b22ff413
austin Imbastari <austinimbastari@gmail.com>
2024-03-05 19:18:56 -0800

    added assets

--- src/components/AboutUs/AboutUs.tsx
+++ src/components/AboutUs/AboutUs.tsx
@@ -1,9 +1,41 @@
 import React from "react";
+import ProgrammingSvg from "../../assets/img/computer.svg";
+import ComputerSvg from "../../assets/img/programming.svg";
 
 const AboutUs = () => {
     return (
         <>
-            <h1 className='text-center'>About Us</h1>
+            <div>
+                <h1 className='text-center'>About Us</h1>
+                <div>
+                    <img src={ProgrammingSvg} alt='programming' />
+                </div>
+                <div>
+                    <p></p>
+                    <div>
+                        <img src='' alt='' />
+                        <h3></h3>
+                        <p></p>
+                    </div>
+                    <div>
+                        <div>
+                            <img src='' alt='Jiggy' />
+                            <h4></h4>
+                            <p></p>
+                        </div>
+                        <div>
+                            <img src='' alt='Good' />
+                            <h4></h4>
+                            <p></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <img src={ComputerSvg} alt='computer-svg' />
+                </div>
+            </div>
         </>
     );
 };
