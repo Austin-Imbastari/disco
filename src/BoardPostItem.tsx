5f731c3abeb651d451690bf3254dd606a7754aa0
austin Imbastari <austinimbastari@gmail.com>
2024-03-14 16:47:00 -0700

    added comments to view from greatest to least and fixed the date the post and comment were created at

--- src/BoardPostItem.tsx
+++ src/BoardPostItem.tsx
@@ -1,33 +1,27 @@
 export const BoardPostItem = ({
-  author,
-  subject,
-  createdDate,
+    author,
+    subject,
+    createdDate,
 }: {
-  author: string;
-  subject: string;
-  createdDate: string;
+    author: string;
+    subject: string;
+    createdDate: string;
 }) => {
-  return (
-    <div className="grid grid-cols-1 gap-5">
-      <div
-        style={{ borderBottom: '2.5px solid #E8F2FE' }}
-        className=""
-      >
-        <div className="flex justify-between px-4 py-10">
-          <div
-            style={{ width: '100%' }}
-            className="flex flex-col"
-          >
-            <div className="flex items-center justify-between ">
-              <h1 className="text-2xl font-bold tracking-wide">{subject}</h1>
-              <div>
-                <h1 className="text-l font-medium">{author}</h1>
-              </div>
+    return (
+        <div className='grid grid-cols-1 gap-5'>
+            <div style={{ borderBottom: "2.5px solid #E8F2FE" }} className=''>
+                <div className='flex justify-between px-4 py-10'>
+                    <div style={{ width: "100%" }} className='flex flex-col'>
+                        <div className='flex items-center justify-between '>
+                            <h1 className='text-2xl font-bold tracking-wide'>{subject}</h1>
+                            <div>
+                                <h1 className='text-l font-medium'>{author}</h1>
+                            </div>
+                        </div>
+                        <p className='font-light tracking-wide'>{createdDate}</p>
+                    </div>
+                </div>
             </div>
-            <p className="font-light tracking-wide">{createdDate}</p>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
--- src/CommentForm.tsx
+++ src/CommentForm.tsx
@@ -1,75 +1,68 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import React, { useContext, useState } from 'react';
-import { UserContext } from './UserContext';
+import React, { useContext, useState } from "react";
+import { UserContext } from "./UserContext";
 
-const CommentForm = ({
-  postId,
-  onCommentSubmitted,
-}: {
-  postId: number;
-  onCommentSubmitted: () => void;
-}) => {
-  const [comment, setComment] = useState('');
-  const currentUser = useContext(UserContext);
+const CommentForm = ({ postId, onCommentSubmitted }: { postId: number; onCommentSubmitted: () => void }) => {
+    const [comment, setComment] = useState("");
+    const currentUser = useContext(UserContext);
 
-  const submitComment = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const newComment = {
-      text: comment,
-      postId: postId,
-      authorId: currentUser?.id ?? 0,
+    const submitComment = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const newComment = {
+            text: comment,
+            postId: postId,
+            authorId: currentUser?.id ?? 0,
+        };
+        try {
+            const url = "https://disco-app-7sxty.ondigitalocean.app/api/comments";
+            const response = await fetch(url, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${localStorage.getItem("auth")}`,
+                },
+                body: JSON.stringify(newComment),
+            });
+            if (response.ok) {
+                const data = await response.json();
+                console.log("Comment submitted successfully:", data);
+                onCommentSubmitted();
+                setComment("");
+            } else {
+                console.error("Failed to submit comment:", response.statusText);
+            }
+        } catch (error) {
+            console.log(error);
+        }
     };
-    try {
-      const url = 'https://disco-app-7sxty.ondigitalocean.app/api/comments';
-      const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${localStorage.getItem('auth')}`,
-        },
-        body: JSON.stringify(newComment),
-      });
-      if (response.ok) {
-        const data = await response.json();
-        console.log('Comment submitted successfully:', data);
-        onCommentSubmitted();
-        setComment('');
-      } else {
-        console.error('Failed to submit comment:', response.statusText);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
 
-  const onChangeHandler = (e: React.FormEvent<HTMLTextAreaElement>): void => {
-    setComment(e.currentTarget.value);
-  };
+    const onChangeHandler = (e: React.FormEvent<HTMLTextAreaElement>): void => {
+        setComment(e.currentTarget.value);
+    };
 
-  return (
-    <>
-      <div className="mt-10 flex justify-center">
-        <div className="w-1/2 flex flex-col justify-center items-center">
-          <textarea
-            onChange={onChangeHandler}
-            value={comment}
-            style={{ resize: 'none', outline: 'none' }}
-            className="block mb-4 p-2.5 w-1/2 text-md text-gray-900 bg-gray-50 rounded-lg border h-40"
-            placeholder="Add a comment here... "
-          ></textarea>
-          <form onSubmit={(e) => submitComment(e)}>
-            <div className="container mt-1 mx-auto px-8  h-16">
-              <div className=" ">
-                <button className="bg-mint text-black px-2 py-2 rounded-md border-solid border-2 border-azure hover:bg-azure tracking-wide transition-colors duration-200">
-                  Comment
-                </button>
-              </div>
+    return (
+        <>
+            <div className='mt-10 flex justify-center'>
+                <div className='w-1/2 flex flex-col justify-center items-center'>
+                    <textarea
+                        onChange={onChangeHandler}
+                        value={comment}
+                        style={{ resize: "none", outline: "none" }}
+                        className='block mb-4 p-2.5 w-1/2 text-md text-gray-900 bg-gray-50 rounded-lg border h-40'
+                        placeholder='Add a comment here... '
+                    ></textarea>
+                    <form onSubmit={(e) => submitComment(e)}>
+                        <div className='container mt-1 mx-auto px-8  h-16'>
+                            <div className=' '>
+                                <button className='bg-mint text-black px-2 py-2 rounded-md border-solid border-2 border-azure hover:bg-azure tracking-wide transition-colors duration-200'>
+                                    Comment
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </div>
-          </form>
-        </div>
-      </div>
-    </>
-  );
+        </>
+    );
 };
 
 export default CommentForm;
