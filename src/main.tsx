b03f1d10284e6b5ee938fd381bb79989292a669a
austin Imbastari <austinimbastari@gmail.com>
2024-03-16 15:07:23 -0700

    added small details

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
 );
+
+window.addEventListener("blur", () => {
+    document.title = "We miss you...";
+});
+
+window.addEventListener("focus", () => {
+    document.title = "Lets Disco";
+});
