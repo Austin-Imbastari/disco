794339110466d808b30817ac3e0c4a59009625bb
austin Imbastari <austinimbastari@gmail.com>
2024-03-18 12:44:37 -0700

    added change for admin permissions

--- src/PostPage.tsx
+++ src/PostPage.tsx
@@ -142,13 +142,15 @@ const PostPage = () => {
                             <div className='mt-5'>
                                 <p className='leading-8 ' dangerouslySetInnerHTML={{ __html: post.text }} />
                             </div>
-                            {currentUser?.id === post.author.id || currentUser?.role === "ADMIN" ? (
+                            {currentUser?.id === post.author.id ? (
                                 <div className='flex flex-row-reverse'>
-                                    <form onSubmit={(e) => handleDelete(e, post.id)}>
-                                        <button className='bg-mint text-black px-2 py-2 rounded-md border-solid border-2 border-azure hover:bg-azure tracking-wide transition-colors duration-200'>
-                                            Delete
-                                        </button>{" "}
-                                    </form>
+                                    {currentUser?.role === "ADMIN" && (
+                                        <form onSubmit={(e) => handleDelete(e, post.id)}>
+                                            <button className='bg-mint text-black px-2 py-2 rounded-md border-solid border-2 border-azure hover:bg-azure tracking-wide transition-colors duration-200'>
+                                                Delete
+                                            </button>{" "}
+                                        </form>
+                                    )}
                                     <Link
                                         to={`/post/${post.id}/edit`}
                                         state={{ title: post.subject, body: post.text, postId: post.id }}
