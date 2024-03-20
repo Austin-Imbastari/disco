25385f877b075adaef1929348f52f74de56c59f8
Ji Park <ji.park@jpnws.com>
2024-03-19 21:07:32 -0700

    Apply page transition to the post page.

--- src/animations.tsx
+++ src/animations.tsx
@@ -21,6 +21,29 @@ export const aboutUsAnimation = {
   },
 };
 
+export const postPageAnimation = {
+  hidden: {
+    opacity: 0,
+    y: -300,
+  },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.5,
+      when: 'beforeChildren',
+      staggerChildren: 0.25,
+    },
+  },
+  exit: {
+    opacity: 0,
+
+    transition: {
+      duration: 0.5,
+    },
+  },
+};
+
 export const titleAnimation = {
   hidden: {
     x: 300,
