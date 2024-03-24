9f6bad9d1c5a803cffa7cc9f525c2de809b34b1f
Ji Park <ji.park@jpnws.com>
2024-03-23 22:47:16 -0700

    Prettify.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-    theme: {
-        extend: {},
-        fontFamily: {
-            sans: ['"Montserrat"', "sans-serif"],
-        },
-        colors: {
-            white: "#ffffff",
-            black: "#000000",
-            mint: "#E9F7E6",
-            azure: "#E8F2FE",
-        },
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: ['"Montserrat"', 'sans-serif'],
     },
-    plugins: [],
+    colors: {
+      white: '#ffffff',
+      black: '#000000',
+      mint: '#E9F7E6',
+      azure: '#E8F2FE',
+    },
+  },
+  plugins: [],
 };
