f9a174bc1fd05c2aceb2967d82adc34795fe0684
austin Imbastari <austinimbastari@gmail.com>
2024-03-26 21:47:27 -0700

    added dark mode to signups

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -133,19 +133,21 @@ const SignIn = () => {
   return (
     <>
       <form onSubmit={handleOnSubmit}>
-        <div className="bg-[#F9FAFB] h-screen w-screen flex items-center">
-          <div className="h-max mx-auto flex flex-col items-center">
-            <h1 className="text-xl font-bold text-center pb-10">Login</h1>
-            <div className="bg-white shadow-xl p-10 flex flex-col gap-4 text-sm">
+        <div className="bg-[#F9FAFB] h-screen w-screen flex items-center dark:bg-darkBg ">
+          <div className="h-max mx-auto flex flex-col items-center ">
+            <h1 className="text-xl font-bold text-center pb-10 dark:text-white">
+              Login
+            </h1>
+            <div className="bg-white shadow-xl p-10 flex flex-col gap-4 text-sm dark:bg-[#1D2023]">
               <div>
                 <label
-                  className="text-gray-600 font-bold inline-block pb-2"
+                  className="text-gray-600 font-bold inline-block pb-2 dark:text-white "
                   htmlFor="username"
                 >
                   Username
                 </label>
                 <input
-                  className="border border-gray-400 focus:outline-slate-400 rounded-md w-full shadow-sm px-5 py-2"
+                  className="border border-gray-400 focus:outline-slate-400 rounded-md w-full shadow-sm px-5 py-2 dark:bg-[#1D2023] dark:text-white"
                   type="username"
                   name="username"
                   placeholder=""
@@ -155,13 +157,13 @@ const SignIn = () => {
               </div>
               <div>
                 <label
-                  className="text-gray-600 font-bold inline-block pb-2"
+                  className="text-gray-600 font-bold inline-block pb-2 dark:text-white"
                   htmlFor="password"
                 >
                   Password
                 </label>
                 <input
-                  className="border border-gray-400 focus:outline-slate-400 rounded-md w-full shadow-sm px-5 py-2"
+                  className="border border-gray-400 focus:outline-slate-400 rounded-md w-full shadow-sm px-5 py-2 dark:bg-[#1D2023] dark:text-white"
                   type="password"
                   name="password"
                   value={credentials.password}
@@ -170,35 +172,28 @@ const SignIn = () => {
                   autoComplete="on"
                 />
               </div>
-              {/* <div className='flex'>
-                                <div className='w-full'>
-                                    <a className='font-bold text-blue-600' href=''>
-                                        Forgot password?
-                                    </a>
-                                </div>
-                            </div> */}
               <div>
                 <input
-                  className="bg-[#E9F7E6] w-full py-2 rounded-md text-black font-bold cursor-pointer hover:bg-[#E8F2FE]"
+                  className="bg-[#E9F7E6] w-full py-2 rounded-md text-black font-bold cursor-pointer hover:bg-[#E8F2FE] dark:text-black dark:bg-darkP"
                   type="submit"
                   value="Login"
                 />
               </div>
               <div>
-                <p className="text-center">Or continue with</p>
+                <p className="text-center dark:text-white">Or continue with</p>
               </div>
               <div className="flex gap-4">
                 <button
                   onClick={handleDemoUser}
-                  className="bg-[#E8F2FE] w-1/2 py-1 rounded-md text-black font-bold cursor-pointer hover:bg-[#E9F7E6]"
+                  className="bg-[#E8F2FE] w-1/2 py-1 rounded-md text-black font-bold cursor-pointer hover:bg-[#E9F7E6] dark:text-black dark:bg-darkP"
                 >
-                  Demo User
+                  Demo
                 </button>
                 <button
                   onClick={handleAdminUser}
-                  className="bg-[#E8F2FE] w-1/2 py-1 rounded-md text-black font-bold cursor-pointer hover:bg-[#E9F7E6]"
+                  className="bg-[#E8F2FE] w-1/2 py-1 rounded-md text-black font-bold cursor-pointer hover:bg-[#E9F7E6] dark:text-black dark:bg-darkP"
                 >
-                  Admin User
+                  Admin
                 </button>
               </div>
             </div>
